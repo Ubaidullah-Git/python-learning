08ac56ba1e969ae01bcca8b3a077592c95876404
Ubaidullah-Git <00ubaidullah0@gmail.com>
2025-12-26 10:35:58 +0500

    New Challenge

---
+++ Code War Challenges/count-codwar.py
@@ -0,0 +1,17 @@
+def count_sheeps(sheep1):
+    return sheep1.count(True)
+
+sheep_array = [True, True, False, True, False, True]
+print(count_sheeps(sheep_array))
+
+def count_sheeps(sheep):
+    count = 0
+    for s in sheep:
+        if s is True:
+            count += 1
+    return count
+
+
+sheep_array = [True, True, False, True, False, True]
+print(count_sheeps(sheep_array))
+
