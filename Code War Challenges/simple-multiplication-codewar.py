70c9d311efdb5bfb127f6ac0341508590ae51b00
Ubaidullah-Git <00ubaidullah0@gmail.com>
2026-01-04 23:20:18 +0500

    New Codewar Challenge

---
+++ Code War Challenges/simple-multiplication-codewar.py
@@ -0,0 +1,10 @@
+def simple_multiplication(number):
+    if number % 2 == 0:
+        return number*8
+    else:
+        return number*9
+
+print(simple_multiplication(42))
+print(simple_multiplication(4465))
+print(simple_multiplication(98392))
+print(simple_multiplication(9839))
