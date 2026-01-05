03a1f20d595d165aaf30ea7a1fad370a34de5d64
Ubaidullah-Git <00ubaidullah0@gmail.com>
2026-01-05 17:06:42 +0500

    New Codewar Challenge

---
+++ Code War Challenges/descending-order-codewar.py
@@ -0,0 +1,16 @@
+def descending_order(num):
+    if len(list(str(num))) > 1:
+        lst = list(str(num))
+        lst.sort(reverse=True)
+        n = "".join(lst)
+        result = int(n)
+        return result
+    else:
+        return num
+
+n1 = 7
+n2 = 35784
+n3 = 234738234520890
+print(descending_order(n1))
+print(descending_order(n2))
+print(descending_order(n3))
