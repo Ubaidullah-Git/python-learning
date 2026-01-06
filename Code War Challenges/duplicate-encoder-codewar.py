9d8f1befe05b6c3cebe0c66f44bc41e79aeebf63
Ubaidullah-Git <00ubaidullah0@gmail.com>
2026-01-06 21:41:31 +0500

    New Codewar Challenge

--- Code War Challenges/duplicate-encoder-codewar.py
+++ Code War Challenges/duplicate-encoder-codewar.py
@@ -1,6 +1,5 @@
 def duplicate_encode(word):
-    word = word.lower()
-    return "".join("(" if word.count(n) == 1 else ")" for n in word )
+    return "".join("(" if word.lower().count(n) == 1 else ")" for n in word )
 
 print(duplicate_encode("din"))
 print(duplicate_encode("recede"))
