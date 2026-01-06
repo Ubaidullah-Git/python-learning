1a2874e6bc8b95d036da45acda474b55c1bdc105
Ubaidullah-Git <00ubaidullah0@gmail.com>
2026-01-06 21:12:02 +0500

    New Codewar Challenge

---
+++ Code War Challenges/even-or-odd-codewar.py
@@ -0,0 +1,9 @@
+def even_or_odd(n):
+    return "Even" if n%2 == 0 else "Odd"
+
+print(even_or_odd(0))
+print(even_or_odd(1))
+print(even_or_odd(2))
+print(even_or_odd(7))
+print(even_or_odd(42))
+print(even_or_odd(-3))
