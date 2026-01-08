03cd8b4fa6c33cbc52569dfc658ae5e8c0cf7781
Ubaidullah-Git <00ubaidullah0@gmail.com>
2026-01-08 21:26:55 +0500

    New Codewar Challenge

--- Code War Challenges/number-of-people-in-the-bus-codewar.py
+++ Code War Challenges/number-of-people-in-the-bus-codewar.py
@@ -1,5 +1,5 @@
 def number(bus_stops): 
-    return sum(n[0] - n[1] for n in bus_stops)
+    return sum(stops[0] - stops[1] for stops in bus_stops)
 
 print(number([[10, 0], [3, 5], [2, 5]]))
 print(number([[3, 0], [9, 1], [4, 10], [12, 2]]))
