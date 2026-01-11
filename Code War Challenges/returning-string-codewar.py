4273e1384fa5a4594bdfb06879163ee992daf2e2
Ubaidullah-Git <00ubaidullah0@gmail.com>
2026-01-11 18:07:57 +0500

    New Codewar Challenge

--- Code War Challenges/returning-string-codewar.py
+++ Code War Challenges/returning-string-codewar.py
@@ -4,6 +4,9 @@ def greet1(name):
 def greet2(name):
     return f"Hello, {name} how are you doing today?"
 
+def greet3(name):
+    return "Hello, {} how are you doing today?".format(name)
+
 print(greet1("Alice"))
 print(greet2("Bob"))
-print(greet2("Zara"))
+print(greet3("Zara"))
