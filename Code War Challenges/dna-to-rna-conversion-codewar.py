4311bc5fcab4ba1a1e5915203a2aa28c0bf4fb5a
Ubaidullah-Git <00ubaidullah0@gmail.com>
2026-01-13 21:27:58 +0500

    New Codewar Challenge

--- Code War Challenges/dna-to-rna-conversion-codewar.py
+++ Code War Challenges/dna-to-rna-conversion-codewar.py
@@ -1,4 +1,8 @@
 def dna_to_rna(dna):
     return dna.translate(str.maketrans("GCAT","GCAU"))
 
+def dna_to_rna1(dna):
+    return dna.replace('T','U')
+
 print(dna_to_rna("GATCGGGTACAGTACTAGACAGATAAAACGTCCGGGATTAGC"))
+print(dna_to_rna1("TGCAGTACTAGATAAAACGTCCGGGATTGC"))
