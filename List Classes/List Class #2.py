1edd53c1773134703861b534437ebca2fcdf30c2
Ubaidullah-Git <00ubaidullah0@gmail.com>
2026-01-14 18:23:13 +0500

    New Codewar Challenge

---
+++ List Classes/List Class #2.py
@@ -0,0 +1,25 @@
+# use of append()
+
+l = [2,4,5,6,7]
+print(l)
+l.append(9)
+print(l)
+
+# use of sort()
+
+lst = [3,5,2,7,1,4,6,7,43,65,66,45,33,56,34,44,32,24,36,33,21,25,14,16]
+print(lst)
+lst.sort()
+print(lst)
+lst.sort(reverse=True)
+print(lst)
+
+# use of reverse()
+
+ls = [3,5,5,6,3,6,2,77,54,77,75,24,43,23,21,16,16,14,134,46,35,67,34]
+print(ls)
+ls.reverse()
+print(ls)
+
+
+
