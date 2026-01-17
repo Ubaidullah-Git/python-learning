00878abec809f44274ddbbcb834b62f2f4db4e47
Ubaidullah-Git <00ubaidullah0@gmail.com>
2026-01-17 18:21:16 +0500

    New Codewar Challenge

---
+++ Code War Challenges/convert-number-into-string.py
@@ -0,0 +1,12 @@
+def number_to_string(num):
+    return f"{num}"
+a = 342
+b = 968
+c = 94
+a = number_to_string(a)
+b = number_to_string(b)
+c = number_to_string(c)
+print(a)
+print(b)
+print(c)
+print(a+b+c)
